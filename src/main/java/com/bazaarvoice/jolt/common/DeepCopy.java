a6a82f48e0f5758d5ee220017049cb68bd56a08a
milo.simpson <milo.simpson@gmail.com>
2012-10-30 16:59:30 -0500

    Code read updates.

--- src/main/java/com/bazaarvoice/jolt/common/DeepCopy.java
+++ src/main/java/com/bazaarvoice/jolt/common/DeepCopy.java
@@ -8,6 +8,17 @@ import java.io.ObjectOutputStream;
 
 public class DeepCopy {
 
+    /**
+     * Simple deep copy, that leverages Java Serialization.
+     * Supplied object is serialized to an in memory buffer (byte array),
+     *  and then a new object is reconstituted from that byte array.
+     *
+     * This is meant for copying small objects or object graphs, and will
+     *  probably do nasty things if asked to copy a large graph.
+     *
+     * @param object object to deep copy
+     * @return deep copy of the object
+     */
     public static Object simpleDeepCopy( Object object ) {
 
         try {
@@ -24,10 +35,10 @@ public class DeepCopy {
             return new ObjectInputStream(bais).readObject();
         }
         catch ( IOException ioe ) {
-            throw new RuntimeException( "DeepCopy IOException" );
+            throw new RuntimeException( "DeepCopy IOException", ioe );
         }
         catch ( ClassNotFoundException cnf ) {
-            throw new RuntimeException( "DeepCopy ClassNotFoundException" );
+            throw new RuntimeException( "DeepCopy ClassNotFoundException", cnf );
         }
     }
 }
