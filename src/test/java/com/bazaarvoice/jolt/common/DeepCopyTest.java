52a794ced13cbea32580c1a12e65733cbdb935ca
milo.simpson <milo.simpson@gmail.com>
2012-10-30 16:59:30 -0500

    Code read updates.

--- src/test/java/com/bazaarvoice/jolt/common/DeepCopyTest.java
+++ src/test/java/com/bazaarvoice/jolt/common/DeepCopyTest.java
@@ -14,9 +14,12 @@ public class DeepCopyTest {
 
         Object input = JsonUtils.jsonToObject( DeepCopyTest.class.getResourceAsStream( "/json/deepcopy/original.json" ) );
 
+        Map<String, Object> fiddle = (Map<String, Object>) DeepCopy.simpleDeepCopy( input );
+
+        JoltTestUtil.runDiffy( "Verify that the DeepCopy did in fact make a copy.", input, fiddle );
+
         // The test is to make a deep copy, then manipulate the copy, and verify that the original did not change  ;)
         // copy and fiddle
-        Map<String, Object> fiddle = (Map<String, Object>) DeepCopy.simpleDeepCopy( input );
         List array = (List) fiddle.get( "array" );
         array.add( "c" );
         array.set( 1, 3 );
@@ -24,12 +27,12 @@ public class DeepCopyTest {
         subMap.put("c", "c");
         subMap.put("b", 3 );
 
-        // Verify we made the modifications we wanted to.
-        Object expectedModified = JsonUtils.jsonToObject( DeepCopyTest.class.getResourceAsStream( "/json/deepcopy/modifed.json" ) );
-        JoltTestUtil.runDiffy( "Verify post deepcopy object looks correct.", expectedModified, fiddle );
-
         // Verify that the input to the copy was unmodified
         Object unmodified = JsonUtils.jsonToObject( DeepCopyTest.class.getResourceAsStream( "/json/deepcopy/original.json" ) );
-        JoltTestUtil.runDiffy( "Verify deepcopy input is unmodifed", unmodified, input );
+        JoltTestUtil.runDiffy( "Verify that the deepcopy was actually deep / input is unmodified", unmodified, input );
+
+        // Verify we made the modifications we wanted to.
+        Object expectedModified = JsonUtils.jsonToObject( DeepCopyTest.class.getResourceAsStream( "/json/deepcopy/modifed.json" ) );
+        JoltTestUtil.runDiffy( "Verify fiddled post deepcopy object looks correct / was modifed.", expectedModified, fiddle );
     }
 }
