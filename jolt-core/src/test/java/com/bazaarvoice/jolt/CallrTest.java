8e5624e2e5f389176d6a9184cc4440a92ea011d8
Mario Molina <mmolimar@gmail.com>
2017-02-18 19:20:31 -0600

    Tests for Callr transformation

---
+++ jolt-core/src/test/java/com/bazaarvoice/jolt/CallrTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2013 Bazaarvoice, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bazaarvoice.jolt;
+
+import com.bazaarvoice.jolt.exception.SpecException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class CallrTest {
+
+    @DataProvider
+    public Object[][] getTestCaseUnits( ) {
+        return new Object[][]{
+                {"firstSample"},
+                {"array_canHandleTopLevelArray"},
+                {"array_canPassThruNestedArrays"},
+                {"starsOfStars"},
+        };
+    }
+
+    @Test( dataProvider = "getTestCaseUnits" )
+    public void runTestUnits( String testCaseName ) throws IOException {
+
+        String testPath = "/json/callr/" + testCaseName;
+        Map<String, Object> testUnit = JsonUtils.classpathToMap( testPath + ".json" );
+
+        Object input = testUnit.get( "input" );
+        Object spec = testUnit.get( "spec" );
+        Object expected = testUnit.get( "expected" );
+
+        Callr callr = new Callr( spec );
+        Object actual = callr.transform( input );
+
+        JoltTestUtil.runDiffy( "failed case " + testPath, expected, actual );
+    }
+
+    @DataProvider
+    public Object[][] getNegativeTestCaseNames( ) {
+        return new Object[][]{
+                {"negativeTestCases"}
+        };
+    }
+
+    @Test( dataProvider = "getNegativeTestCaseNames", expectedExceptions = SpecException.class )
+    public void runNegativeTestCases( String testCaseName ) throws IOException {
+
+        String testPath = "/json/callr/" + testCaseName;
+        Map<String, Object> testUnit = JsonUtils.classpathToMap( testPath + ".json" );
+
+        Object spec = testUnit.get( "spec" );
+        new Callr( spec );
+    }
+}
