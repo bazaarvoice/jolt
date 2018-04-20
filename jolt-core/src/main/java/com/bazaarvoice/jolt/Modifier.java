9d5525486f02f7bc9a48080b952898a4bfaf488d
Andrew Brathovde <abrathovde@cvent.com>
2018-04-20 15:51:03 -0400

    merging leftPad and rightPad into one

--- jolt-core/src/main/java/com/bazaarvoice/jolt/Modifier.java
+++ jolt-core/src/main/java/com/bazaarvoice/jolt/Modifier.java
@@ -46,8 +46,7 @@ public abstract class Modifier implements SpecDriven, ContextualTransform {
         STOCK_FUNCTIONS.put( "concat", new Strings.concat() );
         STOCK_FUNCTIONS.put( "join", new Strings.join() );
         STOCK_FUNCTIONS.put( "split", new Strings.split() );
-        STOCK_FUNCTIONS.put( "leftPad", new Strings.leftPad() );
-        STOCK_FUNCTIONS.put( "rightPad", new Strings.rightPad() );
+        STOCK_FUNCTIONS.put( "pad", new Strings.pad() );
 
         STOCK_FUNCTIONS.put( "min", new Math.min() );
         STOCK_FUNCTIONS.put( "max", new Math.max() );
--- jolt-core/src/main/java/com/bazaarvoice/jolt/modifier/function/Strings.java
+++ jolt-core/src/main/java/com/bazaarvoice/jolt/modifier/function/Strings.java
@@ -85,20 +85,19 @@ public class Strings {
       }
     }
 
-    public static final class leftPad extends Function.ArgDrivenListFunction<String>{
+    public static final class pad extends Function.ArgDrivenListFunction<String>{
         @Override
         protected Optional<Object> applyList(String filler, List<Object> args) {
 
             if(filler == null || args == null) {
                 return Optional.of("");
             } else {
-                if(args.size() == 2) {
-                    Object widthObj = args.get(0);
-                    Object sourceObj = args.get(1);
-                    if(widthObj instanceof Integer && sourceObj instanceof String) {
-                        int width = (Integer) widthObj;
-                        if(filler.length() == 1) {
-                            String source = (String) sourceObj;
+                if(filler.length() == 1 && args.size() == 2) {
+                    Object arg1 = args.get(0);
+                    Object arg2 = args.get(1);
+                    if(arg1 instanceof Integer && arg2 instanceof String) {
+                            int width = (Integer) arg1;
+                            String source = (String) arg2;
                             if(source.length() >= width) {
                                 return Optional.of(source);
                             } else {
@@ -114,44 +113,23 @@ public class Strings {
                                 }
                                 return Optional.of(new String(destinationArray));
                             }
-                        }
-                    }
-                }
-            }
-            return Optional.of("");
-        }
-    }
-
-    public static final class rightPad extends Function.ArgDrivenListFunction<String> {
-
-        @Override
-        protected Optional<Object> applyList(String filler, List<Object> args) {
-
-            if(filler == null || args == null) {
-                return Optional.of("");
-            } else {
-                if(args.size() == 2) {
-                    Object widthObj = args.get(0);
-                    Object sourceObj = args.get(1);
-                    if(widthObj instanceof Integer && sourceObj instanceof String) {
-                        int width = (Integer) widthObj;
-                        if(filler.length() == 1) {
-                            String source = (String) sourceObj;
-                            if(source.length() >= width) {
-                                return Optional.of(source);
-                            } else {
-                                char[] sourceArray = source.toCharArray();
-                                char[] destinationArray = new char[width];
-                                int destIndex = 0;
-                                for(int i = 0; i <= sourceArray.length - 1; i++) {
-                                    destinationArray[destIndex] = sourceArray[i];
-                                    destIndex++;
-                                }
-                                for(int i = destIndex; i <= width - 1; i++) {
-                                    destinationArray[i] = filler.charAt(0);
-                                }
-                                return Optional.of(new String(destinationArray));
+                    } else if(arg1 instanceof String && arg2 instanceof Integer) {
+                        String source = (String) arg1;
+                        int width = (Integer) arg2;
+                        if(source.length() >= width) {
+                            return Optional.of(source);
+                        } else {
+                            char[] sourceArray = source.toCharArray();
+                            char[] destinationArray = new char[width];
+                            int destIndex = 0;
+                            for(int i = 0; i <= sourceArray.length - 1; i++) {
+                                destinationArray[destIndex] = sourceArray[i];
+                                destIndex++;
                             }
+                            for(int i = destIndex; i <= width - 1; i++) {
+                                destinationArray[i] = filler.charAt(0);
+                            }
+                            return Optional.of(new String(destinationArray));
                         }
                     }
                 }
