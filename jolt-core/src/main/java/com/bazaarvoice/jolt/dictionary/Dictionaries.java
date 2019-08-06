bef184ffa2585c63ffe0160b745d26465eea89ab
Thierry <38243024+TH-HA@users.noreply.github.com>
2019-08-06 17:58:13 +0200

    add comment and license header

--- jolt-core/src/main/java/com/bazaarvoice/jolt/dictionary/Dictionaries.java
+++ jolt-core/src/main/java/com/bazaarvoice/jolt/dictionary/Dictionaries.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Infovista.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bazaarvoice.jolt.dictionary;
 
 import java.util.Map;
@@ -5,23 +20,52 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 
- * Simple dictionary manager. Keep the dictionary in memory allowing them to be used during transformation.
- * Structure is thread safe, we keep only a copy of the given dictionaries. 
+ * Simple dictionary manager. Keep the dictionary in memory allowing them to be
+ * used during transformation. Structure is thread safe, we keep only a copy of
+ * the given dictionaries.
  *
  */
 public class Dictionaries {
 
-	static ConcurrentHashMap<String, Map<String, String>> dictionaries = new ConcurrentHashMap<>();
+	static final ConcurrentHashMap<String, Map<String, String>> dictionaries = new ConcurrentHashMap<>();
 
+	/**
+	 * Safely set a dictionary. Keep a full copy of the dictionary in a
+	 * concurrentHashMap avoiding any issue during usage.
+	 * 
+	 * @param name
+	 * @param dictionary
+	 */
 	public static void setDictionary(String name, Map<String, String> dictionary) {
 		dictionaries.put(name, new ConcurrentHashMap<String, String>(dictionary));
 	}
 
+	/**
+	 * unsafe set of the dictionary. A reference is kept and use. Concurrent
+	 * exception may occur if not handled properly. But this allow to have
+	 * distributed cache such as EHCache which is implementing the Map interface.
+	 * 
+	 * @param name
+	 * @param dictionary
+	 */
+	public static void setUnsafeDictionary(String name, Map<String, String> dictionary) {
+		dictionaries.put(name, dictionary);
+	}
+
+	/**
+	 * Remove a dictionary from memory.
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public static boolean removeDictionary(String name) {
 
 		return (dictionaries.remove(name) != null);
 	}
 
+	/**
+	 * Remove all the dictionary from memory.
+	 */
 	public static void removeAll() {
 
 		dictionaries.clear();
